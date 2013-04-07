bb6206aa668e26f96c83eb781fa517366327e8c9
Chris Fordham <chris@xhost.com.au>
2013-04-07 13:25:40 +1000

    Separate strap recipes from local recipes.

--- cookbooks/resolver/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default['resolver']['search'] = `hostname -d`    # assumes valid domain name from fqdn chef requirement
-default['resolver']['nameservers'] = nil
-#default['resolver']['nameservers'] = [ '8.8.8.8', '4.2.2.1' ]
